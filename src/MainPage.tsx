91b1405e910b63b661cb239f0259a68cd974e64c
KasperJordt <kfj@cn3.dk>
2023-04-09 14:27:13 +0200

    cleaned up file structure

---
+++ src/MainPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function MainPage() {
+  return (
+    <div className="MainPage">
+      <header className="Practice Manager">
+        <p>
+          Start
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default MainPage;
